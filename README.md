8c789fe552eaf07168e00963775cad95cef59a54
efeminella <efeminella@gmail.com>
2013-01-06 18:57:20 -0500

    Comments and formatting.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-requirejs-function-module-example
+RequireJS Function Module Example
 =================================
 
 A basic example of implementing Modules as Functions in RequireJS
--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-/*
- * Copyright (c) 2013 Eric Feminella, http://code.ericfeminella.com/license/LICENSE.txt
- */
+
 requirejs.config({
 	paths : {
-        'jquery'        : '../vendor/jquery/1.8.3/jquery.min'
-      , 'underscore'    : '../vendor/lodash/1.0.0-rc.3/lodash.min'
-      , 'backbone'      : '../vendor/backbone/0.9.9/backbone-min'
-      , 'random'        : 'app/utils/random'
-      , 'User'          : 'app/models/User'
-      , 'Roles'         : 'app/models/Roles'
-      , 'AppView'       : 'app/views/AppView'
-      , 'editors'       : 'app/views/editors'
+        'jquery'     : '../vendor/jquery/1.8.3/jquery.min'
+      , 'underscore' : '../vendor/lodash/1.0.0-rc.3/lodash.min'
+      , 'backbone'   : '../vendor/backbone/0.9.9/backbone-min'
+      , 'random'     : 'app/utils/random'
+      , 'User'       : 'app/models/User'
+      , 'Roles'      : 'app/models/Roles'
+      , 'AppView'    : 'app/views/AppView'
+      , 'editors'    : 'app/views/editors'
 	},
     shim: {
         'backbone': {
@@ -21,13 +19,11 @@ requirejs.config({
 });
 
 require(['random'], function(random) {
-    console.log(random(1,100));
-    console.log(random(10,100));
-    console.log(random(50,100));
+    console.log(random(1,10));
 });
 
-require( ['jquery', 'User', 'AppView'], function($, User, AppView) {
-	$( function() {
+require(['jquery', 'User', 'AppView'], function($, User, AppView) {
+	$(function() {
 		new AppView({'model': new User()}).render();
 	});
 });
